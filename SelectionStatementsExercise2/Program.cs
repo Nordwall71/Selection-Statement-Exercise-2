9d4c9a7e7c66b00187db303b8269979f2f2ce4bb
John Nordwall <johnfromchicago@gmail.com>
2023-10-02 20:22:26 -0500

    finally got it

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,46 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("What is your favorite subject?");
+            
+            string faveSubject = Console.ReadLine();
+            
+            
+            
+
+            switch (faveSubject)
+                
+                
+
+               
+            
+            {
+
+                case "Math":
+                    Console.WriteLine("Math is too tough for me!");
+                    break;
+                case "Gym":
+                    Console.WriteLine("You can't be serious...");
+                    break;
+                case "Lunch":
+                    Console.WriteLine("I can't with you.");
+                    break;
+                case "Science":
+                    Console.WriteLine("Now we're getting somewhere!");
+                    break;
+                case "Reading":
+                    Console.WriteLine("Sounds good to me...");
+                    break;
+                default: Console.WriteLine("I'm not familiar with that at all.");
+                    break;
+
+            
+            }
+        }       
     }
 }
